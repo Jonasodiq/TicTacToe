a21ae08cff04e0fd78a194eb96ad85ae50fd7838
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-03 16:13:33 +0200

    Test printBoard() and outdated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        Player p1 = new Player("Per", "X");
+        Player p2 = new Player("Bosse", "O");
+        GameBoard game = new GameBoard(p1, p2);
 
 
     }
