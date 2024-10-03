e8a3c4232d531b77297e41b4f9d8f70c7b518989
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-03 16:10:25 +0200

    Added GameBoard, var. Cnst and printBoard

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 public class GameBoard {
+    String[] board = new String[9];
+    Player p1;
+    Player p2;
+    Scanner input = new Scanner(System.in);
+
+    // Const
+    public GameBoard(Player p1, Player p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+        printBoard(); // Temp
+    }
+
+    // Metod
+    private void printBoard() {
+        System.out.println("\nCurrent game board");
+        for (int i = 0; i < 9; i += 3) {
+            System.out.println(" 1 | 2 | 3");
+            if (i < 6) { // Bara 2 rad
+                System.out.println("---+---+---");
+            }
+        }
+        System.out.println();
+    }
+
 
 }
 
@@ -12,5 +38,4 @@ public class GameBoard {
     - sc
     + startGame()
     + printBoard()
-
  */
