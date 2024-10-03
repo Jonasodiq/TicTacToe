76c28798dc95fa156d5d16ea69746b44c821286d
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-03 14:22:51 +0200

    Added Player, var. and getters

---
+++ src/Player.java
@@ -0,0 +1,38 @@
+public class Player {
+    String name;
+    String symbol;
+    int wins;
+
+    // Const
+    public Player(String name, String symbol, int wins) {
+        this.name = name;
+        this.symbol = symbol;
+        this.wins = 0;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+}
+
+/*TODO Pseudokod
+
+   # Player
+    - name
+    - symbol
+    - wins
+    + getName()
+    + getSymbol()
+    + addWins()
+    + getWins()
+
+ */
