26b18ca727d82ce3648c241756281d9becf13c80
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-10-05 19:26:05 +0200

    Added switchPlayer() and tested

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -5,43 +5,53 @@ public class GameBoard {
     String[] board = new String[9];
     Player p1;
     Player p2;
+    Player currentPlayer;
     Scanner input = new Scanner(System.in);
 
     // Const
     public GameBoard(Player p1, Player p2) {
         this.p1 = p1;
         this.p2 = p2;
+        this.currentPlayer = p1;
         resetBoard();
     }
 
-    // Metod
+    // Metod start the game
     public void startGame() {
         System.out.println("Player 1: " + p1.getName() + " " + p1.getSymbol());
         System.out.println("Player 2: " + p2.getName() + " " + p2.getSymbol());
         System.out.println("Let the game begin! ⚔️");
 
-        showBoard(); // Temp
-
+        boolean winner = false; // Temp
+        if (!winner) {
+            switchPlayer();
+            showBoard(); // Temp
+            System.out.println("Player 1: " + currentPlayer.getName() + " " + currentPlayer.getSymbol());
+        }
     }
 
-    // Metod resetBoard link-> https://www.w3schools.com/java/ref_string_valueof.asp
+    // Reset the game board link-> https://www.w3schools.com/java/ref_string_valueof.asp
     public void resetBoard() { // Lägg till nummer från 1 till 9
         for (int i = 0; i < 9; i++) {
             board[i] = String.valueOf(i + 1);
         }
     }
 
-    // Metod show Board
+    // Method to print the game board
     private void showBoard() {
-        System.out.println("\n Game board");
+        System.out.println("\n\t\tTic-Tac-Toe");
         for (int i = 0; i < 9; i += 3) {
-            System.out.println(" " + board[i] + " | " + board[i + 1] + " | " + board[i + 2]);
+            System.out.println("\t\t" + board[i] + " | " + board[i + 1] + " | " + board[i + 2]);
             if (i < 6) { // Bara 2 rad
-                System.out.println("---+---+---");
+                System.out.println("\t\t-----------");
             }
         }
     }
 
+    // Method to switch turns between players
+    public void switchPlayer() {
+        currentPlayer = currentPlayer == p1 ? p2 : p1;
+    }
 
 
 
@@ -54,7 +64,9 @@ public class GameBoard {
     - p1
     - p2
     - sc
+    - currentPlayer
     + startGame()
     + showBoard()
     + resetBoard()
+    + switchPlayer()
  */
